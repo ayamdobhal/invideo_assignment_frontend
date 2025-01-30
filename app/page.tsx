b24fc7d3aaacc2bd400f2f81422652fc7bd4b0f1
ayamdobhal <me@iamdobhal.dev>
2025-01-30 06:09:32 +0530

    feat: add tab switching

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import { Calculator } from "./components/Calculator";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { Calculator } from "./components/Calculator";
+import { ShaderEditor } from "./components/ShaderEditor";
+
+export default function App() {
+  const [activeTab, setActiveTab] = useState<string>("calculator");
+
   return (
     <div className="p-4 max-w-4xl mx-auto">
-      <div className="flex items-center space-x-4 mb-4">
-        <Calculator />
+      <div className="flex space-x-4 mb-4 text-black">
+        <button
+          className={`px-4 py-2 rounded-lg ${
+            activeTab === "calculator"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200"
+          }`}
+          onClick={() => setActiveTab("calculator")}
+        >
+          Rust Calculator
+        </button>
+        <button
+          className={`px-4 py-2 rounded-lg ${
+            activeTab === "shader" ? "bg-blue-500 text-white" : "bg-gray-200"
+          }`}
+          onClick={() => setActiveTab("shader")}
+        >
+          Text-to-Shader
+        </button>
       </div>
+
+      {activeTab === "calculator" ? <Calculator /> : <ShaderEditor />}
     </div>
   );
 }
